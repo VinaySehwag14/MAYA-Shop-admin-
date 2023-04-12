81d07ccf83a43dd035bd25acc26cf875c3ff7a3a
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-04-12 22:44:12 +0530

    added redux logic and redux-persist

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,70 @@
+import { loginFailure, loginStart, loginSuccess } from "./userRedux";
+import { publicRequest, userRequest } from "../requestMethods";
+import {
+  addProductFailure,
+  addProductStart,
+  addProductSuccess,
+  deleteProductFailure,
+  deleteProductStart,
+  deleteProductSuccess,
+  getProductFailure,
+  getProductStart,
+  getProductSuccess,
+  updateProductFailure,
+  updateProductStart,
+  updateProductSuccess,
+} from "./productRedux";
+
+//* user
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await publicRequest.post("/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
+
+// //* products
+export const getProducts = async (dispatch) => {
+  dispatch(getProductStart());
+  try {
+    const res = await publicRequest.get("/products");
+    dispatch(getProductSuccess(res.data));
+  } catch (err) {
+    dispatch(getProductFailure());
+  }
+};
+
+export const deleteProduct = async (dispatch, id) => {
+  dispatch(deleteProductStart());
+  try {
+    //! uncomment if you want to delete product from database and vice versa
+    await userRequest.delete(`/products/${id}`);
+    dispatch(deleteProductSuccess(id));
+  } catch (err) {
+    dispatch(deleteProductFailure());
+  }
+};
+
+export const updateProduct = async (id, data, dispatch) => {
+  dispatch(updateProductStart());
+  try {
+    //* update
+    await userRequest.put("/products/" + id, data);
+    dispatch(updateProductSuccess({ id, product: data }));
+  } catch (err) {
+    dispatch(updateProductFailure());
+  }
+};
+export const addProduct = async (product, dispatch) => {
+  dispatch(addProductStart());
+  try {
+    //* firebase already sending object thats why we don't have to convert product to object
+    const res = await userRequest.post(`/products`, product);
+    dispatch(addProductSuccess(res.data));
+  } catch (err) {
+    dispatch(addProductFailure());
+  }
+};
---
+++ src/redux/userRedux.js
@@ -0,0 +1,77 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
+    loginFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    logout: (state) => {
+      state.currentUser = null;
+    },
+
+    //*DELETE user
+
+    deleteUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    deleteUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products.splice(
+        state.products.findIndex((item) => item._id === action.payload),
+        1
+      );
+    },
+    deleteUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+
+    //*UPDATE user
+    updateUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    updateUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products[
+        state.products.findIndex((item) => item._id === action.payload.id)
+      ] = action.payload.product;
+    },
+    updateUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+
+    //*ADDING NEW PRODUCT
+    addProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    addProductSuccess: (state, action) => {
+      state.isFetching = false;
+      state.products.push(action.payload);
+    },
+    addProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export default userSlice.reducer;
